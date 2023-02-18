7126d0a1d46c0e9ce18d1cfd030bbfc3e4c01b3d
Get17 <johndoe@example.com>
2023-02-18 14:40:01 -0600

    Ya CASI está jeje

--- Algoritmo_Busqueda_1.0.py
+++ Algoritmo_Busqueda_1.0.py
@@ -1,4 +1,82 @@
+from Basura_Random_1 import area, cant_Basura
+
+
 #Variables base que estaremos usando para controlar el robot
-bateria = 1000
+bateria = [1000]
 movimiento = 10
 recoger = 20
+posicion_Actual = [0,0]
+posicion_Ultima = []
+cambio_Fila = 0
+
+def bateria_Baja():
+    #Determinar cual de los parámetros de coordenada es mayor
+    if posicion_Actual[0] > posicion_Actual[1]:
+        cor_Max = posicion_Actual[0]
+    else: cor_Max = posicion_Actual[1]
+
+    for i in range(cor_Max):
+        if posicion_Actual[0] > 0: 
+            posicion_Actual[0] -= 1
+        if posicion_Actual[1] > 0: 
+            posicion_Actual[1] -= 1
+        print(posicion_Actual)
+        bateria[0] -= movimiento
+        print(bateria[0])
+        
+
+    bateria[0] = 1000
+    volver_Ultima()
+
+def volver_Ultima():
+
+    #Determinar cual de los parámetros de coordenada es mayor
+    if posicion_Ultima[0] > posicion_Ultima[1]:
+        cor_Max = posicion_Ultima[0]
+    else: cor_Max = posicion_Ultima[1]
+
+    for i in range(cor_Max):
+        if posicion_Actual[0] < posicion_Ultima[0]:
+            posicion_Actual[0]+= 1
+        if posicion_Actual[1] < posicion_Ultima[1]:
+            posicion_Actual[1]+= 1
+        print(posicion_Actual)
+        bateria[0] -= movimiento
+        print(bateria[0])
+
+for i in range(7):
+    posicion_Actual[0] = i
+    for j in range(7):
+        if i is not 0 or j is not 0:
+            bateria[0] -= movimiento
+            if cambio_Fila is 0:
+                if i % 2 == 0: 
+                    posicion_Actual[1] += 1
+                else: posicion_Actual[1] -= 1
+            cambio_Fila = 0
+
+            #Comprobación de basura (Recoger)
+            if area[posicion_Actual[0]][posicion_Actual[1]] is 1:
+                area[posicion_Actual[0]][posicion_Actual[1]] = 0
+                bateria[0] -= recoger
+                cant_Basura -= 1
+
+            print(f"Batería: {bateria}")
+            print(f"Basura: {cant_Basura}")
+
+            for z in range(0, len(area[0])):
+                print(area[z])
+
+            print()
+
+            if cant_Basura == 0: 
+                print("Ta todo limpio :) ")
+        
+            if bateria[0] <= 350:
+                posicion_Ultima = posicion_Actual.copy()
+                print("Batería Baja")
+                bateria_Baja()
+
+    cambio_Fila = 1
+
+
--- Basura_Random_1.py
+++ Basura_Random_1.py
@@ -3,24 +3,27 @@ import random
 def basura():
     #Define la cantidad de basura que abrá en el área. Entre 10 y 15 elementos.
     cant_Basura = random.randint(10, 15)
+    print(f"Cantidad de basura: {cant_Basura}")
 
     #Define las posiciones de los elementos (basura)
     for i in range(cant_Basura):
         pos = cord_Random()
-        if area[pos[0]][pos[1]] != 1:
-            area[pos[0]][pos[1]] = 1
-            print(area[pos[0]][pos[1]])
-    
+
+    for i in range(0, len(area[0])):
+        print(area[i])
 
 
+    return(cant_Basura)
+            
 def cord_Random():
     cord_X = random.randint(0,6)
     cord_Y = random.randint(0,6)
     if cord_X == 0 and cord_Y == 0:
         cord_Random()
     else: 
-        cords = [cord_X,cord_Y]
-        return cords
+        if area[cord_X][cord_Y] != 1:
+            area[cord_X][cord_Y] = 1
+        else: cord_Random()
 
 
 area = [[2, 0, 0, 0, 0, 0, 0],
@@ -31,5 +34,5 @@ area = [[2, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0]]
 
-basura()
-print(area)
+cant_Basura = basura()
+
